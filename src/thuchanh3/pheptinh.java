6c187904a086f3cf7078e495b7a487f1119fa7c3
HoangPhuocNhan <hoangphuocnhan99@gmail.com>
2021-07-29 11:04:53 +0700

    add new project

--- src/thuchanh3/pheptinh.java
+++ src/thuchanh3/pheptinh.java
@@ -3,6 +3,15 @@ package thuchanh3;
 public class pheptinh {
 
 	public static void main(String[] args) {
+
+		Scanner scanner = new Scanner(System.in);
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+         
+        System.out.println("Nhập vào số thứ nhất: ");
+        int firstNumber = scanner.nextInt();
+        System.out.println("Nhập vào số thứ hai: ");
+        int secondNumber = scanner.nextInt();
+		// 6 -> 14 Hoàng Phước Nhân
 		// Viết code ở đây nhé, nhớ ghi chú từng phần mỗi người
 	}
 
